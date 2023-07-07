763d75f5269cf3c20ecca0da6338bc747f8bec75
Emir3339 <92637497+Emir3339@users.noreply.github.com>
2023-07-07 10:28:20 +0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,33 @@
-#!/bin/bash
+success=true
 
-apt install python-pip -y
-python3 -m pip -V
-apt install -y software-properties-common
-apt-add-repository --yes --update ppa:ansible/ansible
-apt install -y ansible
-ansible --version
-apt install docker-compose -y
+if ! apt install python-pip -y; then
+    success=false
+fi
 
-echo "complete"
+if [ "$success" = true ]; then
+    python3 -m pip -V;
+
+    if ! apt install -y software-properties-common; then
+        success=false
+    fi
+
+    if [ "$success" = true ]; then
+        apt-add-repository --yes --update ppa:ansible/ansible;
+        apt install -y ansible;
+        ansible --version;
+
+        if ! apt install docker-compose -y; then
+            success=false
+        fi
+
+        if [ "$success" = true ]; then
+            docker-compose -f docker-compose.yaml up -d;
+        fi
+    fi
+fi
+
+if [ "$success" = true ]; then
+    echo "complete"
+else
+    echo "Installation failed. Aborting."
+fi
