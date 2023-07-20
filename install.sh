c13184cc7e7656ef54164747363450e116421f78
Emir3339 <92637497+Emir3339@users.noreply.github.com>
2023-07-20 16:58:47 +0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,6 +6,18 @@ if ! apt install docker-compose -y; then
     success=false
 fi
 
+if systemctl start docker; then
+    echo "Service started successfully."
+    systemctl status docker
+    status=$(systemctl is-active docker)
+    if [ "$status" = "active" ]; then
+    else
+       echo "Service is not active. Aborting."
+    fi
+else
+    echo "Failed to start service. Aborting."
+fi
+
 if [ "$success" = true ]; then
     docker-compose -f docker-compose1.yaml up -d
     echo "Installation complete."
